aa568249ae0889b7ace90a498d80686ee7515944
nikolay.stanev <nikolaygstanev@gmail.com>
2023-07-11 12:15:22 +0300

    Initial commit

---
+++ src/main/java/com/sirmabc/bulkpayments/BulkPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.sirmabc.bulkpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulkPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sirmabc/bulkpayments/BulkPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirmabc.bulkpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
