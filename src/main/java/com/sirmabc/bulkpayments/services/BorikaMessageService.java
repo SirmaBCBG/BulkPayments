02f77f84bba46e458c2e78c14ad06e52201fc9e7
veselin.zinkov <veselin.zinkov@sirmabc.com>
2023-07-25 15:24:16 +0300

    Added sendMessageToBorika method and some other small changes

--- src/main/java/com/sirmabc/bulkpayments/services/BorikaMessageService.java
+++ src/main/java/com/sirmabc/bulkpayments/services/BorikaMessageService.java
@@ -6,13 +6,12 @@ import com.sirmabc.bulkpayments.util.CodesPacs002;
 import com.sirmabc.bulkpayments.util.Header;
 import com.sirmabc.bulkpayments.util.Properties;
 import com.sirmabc.bulkpayments.util.helpers.BulkMessageHelper;
-import com.sirmabc.bulkpayments.util.helpers.XMLHelper;
+import com.sirmabc.bulkpayments.util.helpers.FileHelper;
 import com.sirmabc.bulkpayments.util.xmlsigner.XMLSigner;
 import montranMessage.montran.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -30,14 +29,6 @@ public class BorikaMessageService {
 
     private static final Logger logger = LoggerFactory.getLogger(BorikaMessageService.class);
 
-    @Value("${keystore.sign.path}")
-    private String keyStorePath;
-
-    @Value("${keystore.sign.password}")
-    private String keyStorePassword;
-    @Value("${keystore.sign.alias}")
-    private String keyStoreAlias;
-
     private final BorikaClient borikaClient;
 
     private final Properties properties;
@@ -52,21 +43,21 @@ public class BorikaMessageService {
     }
 
     @Async
-    public void asyncStartProcessingMessage(HttpResponse<String> response) throws AppException {
-        logger.info("Processing message asynchronously..." + Thread.currentThread().getName());
+    public void asyncProcessIncomingMessage(HttpResponse<String> response) throws AppException {
+        logger.info("Asynchronously processing the message that is coming from Borika " + Thread.currentThread().getName());
 
         try {
             Map<String, List<String>> headers = response.headers().map();
             acknowledge(headers);
 
-            Message message = XMLHelper.deserializeXml(response.body(), Message.class);
+            Message message = FileHelper.deserializeXml(response.body(), Message.class);
 
             DatabaseService.saveBulkMessage(message.getAppHdr(), response.body(), headers.get(Header.X_MONTRAN_RTP_MESSAGE_SEQ).get(0));
             CodesPacs002 codesPacs002 = BulkMessageHelper.validate(message.getAppHdr(), headers, response.body());
 
             // TODO: Change the name generation of the .xml file
             if (codesPacs002 == CodesPacs002.OK01) {
-                XMLHelper.objectToXmlFile(message, properties.getBulkMsgsDirPath() + "\\" + UUID.randomUUID() + ".xml");
+                FileHelper.objectToXmlFile(message, properties.getIncmgBulkMsgsDirPath() + "\\" + UUID.randomUUID() + ".xml");
             }
         } catch (Exception e) {
             logger.error(Thread.currentThread().getName() + "threw an error: " + e.getMessage(), e);
@@ -75,20 +66,21 @@ public class BorikaMessageService {
     }
 
     @Async
-    public void asyncStartBuildingMessage(File xmlFile) throws AppException {
-        logger.info("Building message asynchronously..." + Thread.currentThread().getName());
+    public void asyncProcessOutgoingMessage(File xmlFile) throws AppException {
+        logger.info("Asynchronously building the message that is going to Borika " + Thread.currentThread().getName());
 
         try {
-            // TODO: Add PropertiesEntity for the file path
-            XMLHelper.moveFile(xmlFile, "C:\\Users\\veselin.zinkov\\OneDrive - Sirma Business Consulting\\Desktop\\xml_files\\in_progress");
-            Message message = XMLHelper.deserializeXml(xmlFile, Message.class);
+            // TODO: Test file moving
+            FileHelper.moveFile(xmlFile, properties.getOutgngBulkMsgsInProgressDirPath());
+            Message message = FileHelper.deserializeXml(xmlFile, Message.class);
 
             // TODO: Check if everything in the buildAppHdr method is correct
             message.setAppHdr(BulkMessageHelper.buildAppHdr(message));
 
             // TODO: Send the message to Borika
+            Message responseMessage = sendMessageToBorika(message);
         } catch (Exception e) {
-            // TODO: Decide what to do if an error occurs
+            // TODO: Decide what to do with the file if an error occurs
             logger.error(Thread.currentThread().getName() + "threw an error: " + e.getMessage(), e);
             throw new AppException(e.getMessage(), e);
         }
@@ -114,9 +106,24 @@ public class BorikaMessageService {
     }*/
 
     private void acknowledge(Map<String, List<String>> headers) throws IOException, InterruptedException {
-        logger.info("Acknowledging headers...");
+        logger.info("Acknowledging headers");
 
         String msgSeq = headers.get(X_MONTRAN_RTP_MESSAGE_SEQ.header).get(0);
         borikaClient.postAcknowledge(msgSeq);
+    }
+
+    private Message sendMessageToBorika(Message message) throws Exception {
+        logger.info("Sending message to Borika");
+
+        String requestMessageXml = FileHelper.serializeXml(message);
+        String signedRequestMessageXML = FileHelper.signMessage(requestMessageXml, xmlSigner, properties);
+
+        HttpResponse<String> response = borikaClient.postMessage(signedRequestMessageXML);
+
+        //Deserialize response body to java object and get request headers.
+        //Map<String, List<String>> headers = response.headers().map();
+        Message responseMessage = FileHelper.deserializeXml(response.body(), Message.class);
+
+        return responseMessage;
     }
 }
